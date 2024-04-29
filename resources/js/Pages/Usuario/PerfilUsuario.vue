21f8d06ff86daa3a369e3772a1ff8796f4a0b382
RafaDR90 <rafa18220delgado@gmail.com>
2024-04-29 12:11:48 +0200

    reload

--- resources/js/Pages/Usuario/PerfilUsuario.vue
+++ resources/js/Pages/Usuario/PerfilUsuario.vue
@@ -301,14 +301,12 @@ if (props.selectedUser && props.selectedUser.image_url) {
     //compruebo si cambia el estado de props.imgChange y ejecuto la funcion downloadImage
     watch(() => props.imgChange, (newValue) => {
         if (newValue) {
-            console.log(props.imgChange)
             if (props.imgChange) {
-                console.log("inicio timeout")
                 setTimeout(() => {
-                    console.log('ejecuto timeout')
                     downloadImage();
                     props.imgChange = false;
-                }, 500);
+                    Inertia.reload();
+                }, 250);
             }
         }
     });
