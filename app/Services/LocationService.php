30856231e978de5a2be0c4b1bc1f76d739710e02
RafaDR90 <rafa18220delgado@gmail.com>
2024-05-16 16:58:58 +0200

    probando ip

--- app/Services/LocationService.php
+++ app/Services/LocationService.php
@@ -9,6 +9,10 @@ class LocationService
     public static function getLocation()
     {
         $ip = request()->ip();
+        $ip = request()->getClientIp();
+        $ip =$_SERVER['REMOTE_ADDR'];
+        var_dump('asdasd');
+        dd($_SERVER['REMOTE_ADDR']);
 
         //BORRAR ESTO CUANDO SE SUBA A PRODUCCION
         $ip = '154.62.41.89';
