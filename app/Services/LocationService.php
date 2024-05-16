65002b3d6395131673d17dca2388c33a98254e6b
RafaDR90 <rafa18220delgado@gmail.com>
2024-05-16 16:44:54 +0200

    ip fake

--- app/Services/LocationService.php
+++ app/Services/LocationService.php
@@ -11,7 +11,7 @@ class LocationService
         $ip = request()->ip();
 
         //BORRAR ESTO CUANDO SE SUBA A PRODUCCION
-        //$ip = '154.62.41.89';
+        $ip = '154.62.41.89';
         $client = new Client();
         $response = $client->request('GET', 'http://ip-api.com/json/' . $ip);
         return json_decode($response->getBody()->getContents(),true);
